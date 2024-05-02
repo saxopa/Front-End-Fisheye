0510fc2eab8e6b7791217832a9f012a576018770
saxopa <alexispontikis1@gmail.com>
2024-05-02 19:05:04 +0200

    getDetailsPhotographer

--- scripts/pages/index.js
+++ scripts/pages/index.js
@@ -22,6 +22,7 @@ async function init() {
   displayData(photographers);
 }
 
+
 init();
 
 
--- scripts/services/photographerservice.js
+++ scripts/services/photographerservice.js
@@ -8,15 +8,29 @@ export default class PhotographerService {
     try {
       // Récupérer les données des photographes
       const data = await this.getPhotographers();
-
+      let resultat = {media:[]};
       // Recherche dans la liste des photographes
       for (let i = 0; i < data.photographers.length; i++) {
         if (data.photographers[i].id === parseInt(id)) {
-          return Promise.resolve(data.photographers[i]); // Renvoie les détails du photographe
+          resultat.details = data.photographers[i]; // Renvoie les détails du photographe
+          break;
         }
       }
-      // Si aucun photographe avec cet identifiant n'est trouvé, renvoie une promesse rejetée
-      return Promise.reject(new Error("Aucun photographe trouvé avec cet ID"));
+      for (let i = 0; i < data.media.length; i++) {
+        if (data.media[i].photographerId === parseInt(id)) {
+          //Ajoute les élements au tableau média
+          resultat.media.push(data.media[i]); // Renvoie les détails du photographe
+        }
+      }
+
+      if (resultat != {media:[]}) {
+        return Promise.resolve(resultat);
+      } else {
+        // Si aucun photographe avec cet identifiant n'est trouvé, renvoie une promesse rejetée
+        return Promise.reject(
+          new Error("Aucun photographe trouvé avec cet ID")
+        );
+      }
     } catch (error) {
       // Gérer les erreurs de récupération des données des photographes
       return Promise.reject(error);
